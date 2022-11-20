0cb7be09bde7dbd28822f9b1c14607f3bf831f46
AlexMusaev <89597281+AlexMusaev@users.noreply.github.com>
2022-11-20 22:25:19 +0500

    add random

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 45;
-int NumberB = 4;
+﻿int NumberA = new Random().Next(1, 10); //1 2 3 4 5 ... 9
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1, 10); //1 2 3 4 5 ... 9
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
